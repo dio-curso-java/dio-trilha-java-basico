b09e4e89c3a68b502fa33488f980dd9bf7c5d5d7
Rodrigo Roma <rodrigo.rmartins@gmail.com>
2023-05-04 22:44:40 -0300

    Correção da estrutura para rodar corretamente o Java

--- desafios/controle-fluxo/src/Contador.java
+++ desafios/controle-fluxo/src/Contador.java
@@ -8,6 +8,7 @@ import java.util.*;
  */
 
 public class Contador {
+	
 	public static void main(String[] args) {
 		Scanner terminal = new Scanner(System.in);
 		System.out.println("Digite o primeiro parâmetro");
